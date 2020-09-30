e81327a6a122d793bffe9e0d9908aa1d36c654c4
SharonMigot <migotsharon@gmail.com>
2020-09-30 08:44:26 +0300

    made changes

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+import { AlertService } from '../alert-service/alert.service';
+import { Quote } from '../quote-class/quote';
+import { QuoteRequestService } from '../quote-http/quote-request.service';
 
 @Component({
   selector: 'app-goal',
@@ -7,14 +12,12 @@ import { Goal } from '../goal';
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
-  goals: Goal[] = [
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', new Date(2020, 3, 14)),
-    new Goal(2, 'Buy Cookies', 'I have to buy cookies for the parrot', new Date(2019, 6, 9)),
-    new Goal(3, 'Get new Phone Case', 'Diana has her birthday coming up soon', new Date(2022, 1, 12)),
-    new Goal(4, 'Get Dog Food', 'Pupper likes expensive snacks', new Date(2019, 0, 18)),
-    new Goal(5, 'Solve math homework', 'Damn Math', new Date(2019, 2, 14)),
-    new Goal(6, 'Plot my world domination plan', 'Cause I am an evil overlord', new Date(2030, 3, 14)),
-  ];
+ goals:Goal[];
+  alertService:AlertService;
+  quote:Quote;
+
+
+
   addNewGoal(goal) {
     let goalLength = this.goals.length;
     goal.id = goalLength + 1;
@@ -27,6 +30,7 @@ export class GoalComponent implements OnInit {
 
       if (toDelete) {
         this.goals.splice(index, 1)
+        this.alertService.alertMe("The goal has been deleted")
       }
     }
   }
@@ -38,18 +42,17 @@ export class GoalComponent implements OnInit {
       this.goals.splice(index, 1);
     }
   }
-  constructor() { }
 
-  ngOnInit() {
+  constructor(goalService:GoalService, alertService:AlertService, private quoteService:QuoteRequestService) {
+    this.goals = goalService.getGoals()
+    this.alertService = alertService;
   }
 
+  ngOnInit() {
+    this.quoteService.quoteRequest()
+    this.quote = this.quoteService.quote
+  }
 }
-
-
-
-
-
-
 
 
 
